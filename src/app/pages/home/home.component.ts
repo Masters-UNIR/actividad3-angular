8c9eec5c892f5e75a9edc2286fa369d2446eb0dc
inesrivilla <alumno.91172@iesazarquiel.es>
2024-06-19 20:09:00 +0200

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CarrousImagesComponent } from '../../components/carrous-images/carrous-images.component';
+import { firstCourse, main } from '../../../assets/data/data'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CarrousImagesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+ 
+  firstCourse: any[] = [];
+  main: any[] = [];
+
+  constructor() {}
+
+
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData(): void {
+    this.firstCourse = firstCourse;
+    this.main = main;
+  }
 
 }
+
