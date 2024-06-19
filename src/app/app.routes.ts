6f3e13bb85ab68ccddea365990b38ae1655d2b73
inesrivilla <alumno.91172@iesazarquiel.es>
2024-06-19 18:53:02 +0200

    enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,17 @@
 
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { EnviromentComponent } from './pages/enviroment/enviroment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'', component: HomeComponent},
+    {path:'**', component: ErrorComponent},
+    {path:'about', component: AboutComponent},
+    {path:'contact', component: ContactComponent},
+    {path:'enviroment', component: EnviromentComponent},
+
+];
