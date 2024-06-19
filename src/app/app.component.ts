3d0fd11e29d240145c6e804c6c4fd6543f64827d
inesrivilla <alumno.91172@iesazarquiel.es>
2024-06-19 19:20:30 +0200

    fa icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoBarComponent } from './components/info-bar/info-bar.component';
+import { CommonModule } from '@angular/common';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FontAwesomeModule, InfoBarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'actividad3-angular';
+  faCoffee = faCoffee;
 }
--- src/app/components/info-bar/info-bar.component.ts
+++ src/app/components/info-bar/info-bar.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info-bar',
   standalone: true,
-  imports: [],
+   imports: [CommonModule, ],
   templateUrl: './info-bar.component.html',
   styleUrl: './info-bar.component.css'
 })
