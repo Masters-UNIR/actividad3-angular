86ccd2bc4bd59bc3399cdf097b5829b07618f7f3
inesrivilla <alumno.91172@iesazarquiel.es>
2024-06-19 20:24:54 +0200

    cultura

--- src/app/pages/enviroment/enviroment.component.ts
+++ src/app/pages/enviroment/enviroment.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { EntornoNaturalComponent } from '../../components/entorno-natural/entorno-natural.component';
+import { CulturaOcioComponent } from '../../components/cultura-ocio/cultura-ocio.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-enviroment',
   standalone: true,
-  imports: [],
+  imports: [EntornoNaturalComponent,CulturaOcioComponent, CommonModule],
   templateUrl: './enviroment.component.html',
   styleUrl: './enviroment.component.css'
 })
