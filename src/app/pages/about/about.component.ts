544591be2410d510ab7faea25ed38ddb8f83d34b
inesrivilla <alumno.91172@iesazarquiel.es>
2024-06-19 20:16:58 +0200

    sobre nosotros

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { InstalacionesResComponent } from '../../components/instalaciones-res/instalaciones-res.component';
+import { NuestraHistoriaComponent } from '../../components/nuestra-historia/nuestra-historia.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InstalacionesResComponent,NuestraHistoriaComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
